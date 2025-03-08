7d404190ad498ec425894dfd1963329d2766f672
Natalia <natalia.avello.a@gmail.com>
2025-03-07 22:00:59 -0300

    actualiza app

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Array donde se almcenan los participantes del juego 
+let participantes= [];
+/**
+ * Agrega un nuevo participante a la lista.
+ * - Toma el valor del input y lo guarda en el array `participantes`.
+ * - Evita agregar nombres duplicados.
+ * - Luego actualiza la lista mostrada en pantalla.
+ */
+function agregarParticipante() {
+    let nombre = document.getElementById("nombre").value.trim(); //obtiene el nombre y elimina los espacio extras
+    if( n)
+}
